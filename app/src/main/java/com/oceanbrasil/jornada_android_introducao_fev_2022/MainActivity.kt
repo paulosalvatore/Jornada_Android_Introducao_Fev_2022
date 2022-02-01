df37bb4c880b59250adeb742147a5b08666b2af8
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-01 11:51:52 -0300

    Funcionamento do botão implementado

--- app/src/main/java/com/oceanbrasil/jornada_android_introducao_fev_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/jornada_android_introducao_fev_2022/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.oceanbrasil.jornada_android_introducao_fev_2022
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Adicionar um Listener ao botão
+        // 1 - Pegar o botão
+        // 2 - Adicionar o listener
+
+        val button = findViewById<Button>(R.id.button)
+        val textView = findViewById<TextView>(R.id.textView)
+
+        // Dentro das chaves, o código que escrevemos será executado apenas
+        // quando o botão for clicado
+        button.setOnClickListener {
+            // Código para executar quando o botão for clicado
+
+            textView.text = "Samsung Ocean!!"
+        }
     }
 }
