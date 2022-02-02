ba65a787f33a6ea8df0954ba8d0f3171b42e4bee
Paulo Salvatore <salvatoregames@gmail.com>
2022-02-02 11:08:18 -0300

    Funcionamento do botão AbrirNovaTela implementado

--- app/src/main/java/com/oceanbrasil/jornada_android_introducao_fev_2022/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/jornada_android_introducao_fev_2022/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.oceanbrasil.jornada_android_introducao_fev_2022
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -33,6 +34,20 @@ class MainActivity : AppCompatActivity() {
             } else {
                 etName.error = "Digite um nome válido"
             }
+        }
+
+        // Funcionamento para abrir nova tela
+        val btAbrirNovaTela = findViewById<Button>(R.id.btAbrirNovaTela)
+
+        // Sempre que o botão for clicado
+        btAbrirNovaTela.setOnClickListener {
+            // Código será executado quando o botão for clicado
+            // Para abrir uma nova tela, registramos a intenção de fazer isso
+            // Intenção no Android = Intent
+            val intentAbrirNovaTela = Intent(this, ResultadoActivity::class.java)
+
+            // Registramos a Intent
+            startActivity(intentAbrirNovaTela)
         }
     }
 }
